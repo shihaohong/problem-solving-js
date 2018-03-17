3c94cb5d938ac8c437c37f3a4931e77115656422
Akhtam Ismatov <akhtam.ismatov@gmail.com>
2018-03-17 00:16:12 -0700

    lastword basics

--- src/lengthOfLastWord.js
+++ src/lengthOfLastWord.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-
+  //[i]string, [O]number, [C] none, [E]lower&Upper case
+  /* first pseudocode
+turn string into lowercase
+split the input
+get last element
+return length of last element
+*/
+  //split the string and pop last element
+  var lastWord = s.split(' ').pop().length;
+  //return length of last element
+  return lastWord;
 };
